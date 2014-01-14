36cfc71d6dfd03c4c9858b62c165a4318d110b23
vagrant <vagrant@precise64.(none)>
2014-01-14 10:55:54 +0000

    add init and stop script

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-docker run -d -v /home/vagrant/codebox/webapp/app:/home/host -p 9000:9000 -p 35729:35729 -p 9001:9001 -p 7777:7777 codeBox codebox run /home/host -p 7777
+docker run -d -v /home/vagrant/codebox/webapp:/home/host -p 9000:9000 -p 35729:35729 -p 9001:9001 -p 7777:7777 codeBox codebox run /home/host/app -p 7777
 docker run -d -p 80:80 -v /home/vagrant/icecoder:/var/www/project phpTest
 docker run -d -p 3306:3306 -v /home/vagrant/data/mysql:/var/lib/mysql mysql

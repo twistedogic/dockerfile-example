f641504dacd443a8478e3d7b71d8a960b6e38126
vagrant <vagrant@precise64.(none)>
2014-01-30 07:36:49 +0000

    add webdev

--- phpdev.sh
+++ phpdev.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-docker run -d -v /home/vagrant/icecoder:/home/host -p 9002:22 sshd
-docker run -d -p 80:80 -v /home/vagrant/icecoder:/var/www phpTest
+docker run -d -v /home/vagrant/icecoder/projectWork:/home/host -p 9002:22 sshd
+docker run -d -p 80:80 -v /home/vagrant/icecoder/projectWork:/var/www phpTest
 docker run -d -p 3306:3306 -v /home/vagrant/data/mysql:/var/lib/mysql mysql

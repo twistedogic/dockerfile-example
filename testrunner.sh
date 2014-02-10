6b77619112b6fd91cf3af90231e524287c5a252d
vagrant <vagrant@precise64.(none)>
2014-02-10 07:19:28 +0000

    update testrunner startup script

--- testrunner.sh
+++ testrunner.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-docker run -d -v -p 9876:9876 -p 9000:9000 -p 9002:22 -p 35729:35729 /home/vagrant/testrunner:/home/testrunner testrunner
+docker run -d -p 9876:9876 -p 9000:9000 -p 9002:22 -p 35729:35729 -v /home/vagrant/testrunner:/home/testrunner testrunner

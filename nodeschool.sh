3ab59f70a95d987f0b8f1b2f7b37fb596c90041c
twistedogic <twistedogic@gmail.com>
2014-02-18 16:46:13 +0800

    Update nodeschool.sh

--- nodeschool.sh
+++ nodeschool.sh
@@ -1,1 +1,1 @@
-docker run -d -p 3000:3000 -p 9002:22 -v /home/vagrant/nodeschool:/home/nodeschool nodeschool
+docker run -d -p 3000:3000 -p 9002:22 -p 7261:7261 -v /home/vagrant/nodeschool:/home/nodeschool nodeschool

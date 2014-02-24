5e35a7201768a408aeb0a05c87346e717af9a663
vagrant <vagrant@precise64.(none)>
2014-02-24 06:58:23 +0000

    add ghost

---
+++ ghost.sh
@@ -0,0 +1,1 @@
+docker run -d -p 80:2368 -v /home/ghost:/ghost-override dockerfile/ghost
--- nodeschool.sh
+++ nodeschool.sh
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-docker run -d -p 3000:3000 -p 7261:7261 -p 9002:22 -v /home/vagrant/nodeschool:/home/nodeschool nodeschool
-=======
-docker run -d -p 3000:3000 -p 9002:22 -p 7261:7261 -v /home/vagrant/nodeschool:/home/nodeschool nodeschool
->>>>>>> fab1507ed1a73be62a72123e6ec4da296683a737
+
+docker run -d -p 3131:3131  -p 3000:3000 -p 9002:22 -p 7261:7261 -v /home/vagrant/nodeschool:/home/nodeschool nodeschool
